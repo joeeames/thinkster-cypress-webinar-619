674c097fa310ff571a3cdf9ff4b017089dde5c34
Joe Eames <joeeames@gmail.com>
2019-05-23 10:35:33 -0600

    ready for webinar

--- cypress/integration/first-tests.spec.ts
+++ cypress/integration/first-tests.spec.ts
@@ -1,8 +1,4 @@
 describe('Our First Test', () => {
 
-  it('should be on the front page', () => {
-    cy.visit('/');
-    cy.get('h1').contains('Tour of Heroes');
-    cy.get('.module.hero').contains('Mr. Nice');
-  })
+ 
 })
